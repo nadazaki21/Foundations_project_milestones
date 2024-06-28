7e15e303032737d7fb9564e50c9c6792a7c3b18e
nadazkai21 <nadazaki019@gmail.com>
2024-06-28 21:30:12 +0300

    creation form added

--- front-end/src/components/MyProjects/MyProjects.jsx
+++ front-end/src/components/MyProjects/MyProjects.jsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React , {useState} from "react";
 import SingleProjectComponent from "./SingleProjectComponent";
 import Given_Tasks from "./Given_Tasks";
 import "./MyProjects.css";
 import SideButtons from "../DashBoard/SideButtons";
+import NewProjectButton from "../MyProjects/NewProjectButton";
+import NewProjectForm from "../CreationForms/NewProjectForm";
 import { useNavigate } from "react-router-dom";
 const MyProjects = () => {
 
     const navigate = useNavigate();
 
+    const [clicked, setClicked] = useState(false);
+
     return (
+        clicked ? <div className="creation_form">
+            <div className="header">
+                <img  onClick={() => navigate('/')} className="logo" src="https://cdn.dribbble.com/userupload/12196251/file/original-84d2e13b8e5602c0e15a891bb8fdb101.png?resize=400x300&vertical=center" />
+            </div>
+            <div className="button_with_form">
+            <button className="back_button" onClick={() => setClicked(false)} >Back To Projects</button>
+                <NewProjectForm /> 
+            </div>
+        
+            
+        </div>  :
         <div className="all">
            <div className="header">
                 <img  onClick={() => navigate('/')} className="logo" src="https://cdn.dribbble.com/userupload/12196251/file/original-84d2e13b8e5602c0e15a891bb8fdb101.png?resize=400x300&vertical=center" />
             </div>
+            
             <div className="side_buttons">
                 <SideButtons />
             </div>
+            <button className="add_project button" onClick={() => setClicked(true)}> + Create New Project</button>
             <div className="projects_container">
+            
             <SingleProjectComponent />
             </div>
             <div className="given_tasks">
--- front-end/src/components/MyProjects/SingleProjectComponent.jsx
+++ front-end/src/components/MyProjects/SingleProjectComponent.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './SingleProjectComponent.css';
+import { useNavigate } from "react-router-dom";
 
 const SingleProjectComponent = () => {
     return (
         <div className="container">
             <div className="div_header">
                 <h2> My Projects </h2>
-                <button className="add_project"> + Create New Project</button>
+                {/* <button className="add_project"> + Create New Project</button> */}
             </div>
             <div className="projects_container">
                 <div className="project_name">
