e3c465c093e0c565fa6aef67f8bad2c7b30d1818
nadazkai21 <nadazaki019@gmail.com>
2024-07-03 18:19:01 +0300

    creation form project and phases back-end done

--- back-end/app/routes/creation_form_routes.py
+++ back-end/app/routes/creation_form_routes.py
@@ -1,17 +1,22 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from ..wrappers import get_user_if_logged
-from ..models.models import Phase, Project, Task , Subtask
+from ..models.models import Phase, Project, Task , Subtask, User
 from ..database import db
 from datetime import datetime
 
-creationForm_bp = Blueprint('creationForm', __name__, url_prefix='/creation-form')
+creationForm_bp = Blueprint('creationForm', __name__, url_prefix='/creation_form')
 
 
 # routes for viewing an existing project
+
+
+
+
 @creationForm_bp.route('/phases', methods=['GET'], strict_slashes = False)
 @get_user_if_logged
-def get_phases(user_id, project_id):
+def get_phases(user_id):
     """ returns phases of a specific project """
+    project_id = request.args.get('project_id')
     all_phases = Phase.query.filter_by(project_id=project_id).all()
     return jsonify([phase.to_dict() for phase in all_phases])
 
@@ -33,13 +38,41 @@ def get_subtasks_for_task(user_id, task_id):
 
 # routes for creating a new project
 
-@creationForm_bp.route('/add-phases', methods=['POST'], strict_slashes = False)
+
+@creationForm_bp.route('/add_project', methods=['POST'], strict_slashes = False)
 @get_user_if_logged
-def create_new_phase(user_id, project_id,name):
+def create_new_project(user_id):
     """ creates a new phase """
-    phase = Phase(project_id=project_id, name=name)
+    data = request.get_json()
+    name = data.get('name')
+    owners = data.get('owners')
+    members = data.get('members')
+    
+    owners["owners"].append(User.query.filter_by(id=user_id).first()) # adding the logged in user as an owner by default
+    
+    project = Project(name=name["name"], owners=owners["owners"], members=members["members"], creation_date=datetime.utcnow())
+    db.session.add(project)
+    db.session.commit()
+    return jsonify(project.to_dict()) , 201
+
+@creationForm_bp.route('/add_phases', methods=['POST'], strict_slashes = False)
+@get_user_if_logged
+def create_new_phase(user_id):
+    """ creates a new phase """
+    data = request.get_json()
+    name = data.get('name')
+    # print(name)
+    project_id = data.get('project_id')
+    tasks = data.get('tasks')
+    # print(tasks)
+    phase = Phase(project_id=project_id['project_id'], name=name['name'], tasks=tasks)
     db.session.add(phase)
     db.session.commit()
+    
+    # checking if phase is actually added in the project
+    # project = Project.query.filter_by(id=project_id['project_id']).first()
+    # print(f"printing phases {project.phases}")
+    
     return jsonify(phase.to_dict())
 
 @creationForm_bp.route('/add-task', methods=['POST'], strict_slashes = False)
